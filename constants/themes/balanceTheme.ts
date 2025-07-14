92071cf1c8db7392e69cb1ea952a9ba56d7fd7fe
Edan Harr <edan@analyticintelligencesolutions.com>
2025-07-14 04:26:23 -0700

    Updated balanceTheme.ts

--- constants/themes/balanceTheme.ts
+++ constants/themes/balanceTheme.ts
@@ -3,7 +3,7 @@ export const balanceTheme = {
   name: 'Balance Theme',
   id: 'balance',
   backgroundImage: 'https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
-  addTaskIcon: 'leaf', // Add task icon for all tabs in this theme
+  addTaskIcon: 'palette', // Add task icon for all tabs in this theme
   tabColors: {
     // Blue - Daily
     daily: {
