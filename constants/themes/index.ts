c102eca5969e6736066820b362ca42294cd26942
Edan Harr <edan@analyticintelligencesolutions.com>
2025-07-15 16:07:02 -0700

    Updated index.ts

--- constants/themes/index.ts
+++ constants/themes/index.ts
@@ -3,27 +3,57 @@ import { latteTheme } from './latteTheme';
 import { rainstormTheme } from './rainstormTheme';
 import { autumnTheme } from './autumnTheme';
 
-export interface ThemeColors {
-  pastel: string;
+export interface TabColorSet {
+  veryDark: string;
+  shadow: string;
   dark: string;
+  medium: string;
+  pastel: string;
+  bgAlt: string;
+  highlight: string;
   bg: string;
   accent: string;
 }
 
+export interface ColorWheelSet {
+  redBold: string;
+  redLight: string;
+  orangeBold: string;
+  orangeLight: string;
+  yellowBold: string;
+  yellowLight: string;
+  greenBold: string;
+  greenLight: string;
+  blueBold: string;
+  blueLight: string;
+  indigoBold: string;
+  indigoLight: string;
+  purpleBold: string;
+  purpleLight: string;
+  pinkBold: string;
+  pinkLight: string;
+  brownBold: string;
+  brownLight: string;
+  grayBold: string;
+  grayLight: string;
+}
+
 export interface TabColors {
-  daily: ThemeColors;
-  future: ThemeColors;
-  calendar: ThemeColors;
-  meals: ThemeColors;
-  cleaning: ThemeColors;
-  selfCare: ThemeColors;
-  delegate: ThemeColors;
+  daily: TabColorSet;
+  future: TabColorSet;
+  calendar: TabColorSet;
+  meals: TabColorSet;
+  cleaning: TabColorSet;
+  selfCare: TabColorSet;
+  delegate: TabColorSet;
+  themeColorWheel: ColorWheelSet;
 }
 
 export interface Theme {
   name: string;
   id: string;
   backgroundImage?: string;
+  addTaskIcon?: string;
   tabBackgrounds?: {
     [key: string]: string;
   };
